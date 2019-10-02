51c7c4f978f1fce44c3a0599e32b183073d19703
Ildar Zalyalov <ildarzalyalovmos@gmail.com>
2019-10-02 15:52:49 +0300

    Initial Commit

---
+++ TableViewLesson/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TableViewLesson
+//
+//  Created by Ильдар Залялов on 02/10/2019.
+//  Copyright © 2019 Ildar Zalyalov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
